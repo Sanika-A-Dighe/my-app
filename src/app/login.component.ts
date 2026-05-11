1af6302adbf4624fe2fcba21eebb78e2d84601af
Sanika-A-Dighe <141570480+Sanika-A-Dighe@users.noreply.github.com>
2026-05-11 23:17:50 +0530

    updated code in login component

--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -7,25 +7,7 @@ import { Router } from '@angular/router';
   selector: 'app-login',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
-  template: `
-    <form [formGroup]="loginForm" (ngSubmit)="onSubmit()">
-      <div>
-        <label for="email">Email</label>
-        <input id="email" type="email" formControlName="email" />
-        <div *ngIf="isInvalid('email')">Enter a valid email.</div>
-      </div>
-
-      <div>
-        <label for="password">Password</label>
-        <input id="password" type="password" formControlName="password" />
-        <div *ngIf="isInvalid('password')">Password is required.</div>
-      </div>
-
-      <button type="submit">Login</button>
-
-      <div *ngIf="errorMessage">{{ errorMessage }}</div>
-    </form>
-  `
+  templateUrl: './login.component.html'
 })
 export class LoginComponent {
   private readonly formBuilder = inject(FormBuilder);
